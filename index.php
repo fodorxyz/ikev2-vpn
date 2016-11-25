cd0ab68a34fcc0de46bccd3494e9ba24c7767fd6
Ashley Hindle <hey@ashleyhindle.com>
2016-11-25 12:10:13 +0000

    Adds UUID generation for the ikev2.mobileconfig

--- index.php
+++ index.php
@@ -4,6 +4,13 @@ if (isset($_GET['vs'])) {
     exit;
 }
 $template = file_get_contents('configs/ikev2.mobileconfig');
+$uuidReplacement = '{{UID}}';
+$uuidsToReplace = substr_count($template, $uuidReplacement);
+
+for ($i = 0; $i < $uuidsToReplace; $i++) {
+    $uuid = guidv4(openssl_random_pseudo_bytes(16));
+    $template = str_replace($uuidReplacement, $uuid, $template);
+}
 
 $requiredGet = [
     'domain',
@@ -76,3 +83,13 @@ echo str_replace(
     ],
     $template
 );
+
+
+function guidv4($data) {
+    assert(strlen($data) == 16);
+
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
