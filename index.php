6c31d44442d65d0b387c1dcb882aa14f2ec56b09
Ashley Hindle <ashleyhindle@users.noreply.github.com>
2017-06-24 15:05:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,6 @@ $requiredPost = [
     'domain',
     'vpnUsername',
     'vpnSecret',
-    'vpnPassword',
 ];
 $domain = (!empty($_GET['domain'])) ? $_GET['domain'] : '';
 $vpnUsername = (!empty($_GET['vpnUsername'])) ? $_GET['vpnUsername'] : '';
@@ -53,8 +52,6 @@ if (!empty(array_diff($requiredPost, $provided))) {
     <input name='vpnUsername' value='{$vpnUsername}'><br />
     <label>VPN Secret</label><br />
     <input name='vpnSecret' type='password' value=''><br />
-    <label>VPN Password</label><br />
-    <input name='vpnPassword' type='password' value=''><br />
     
     <input type='submit'>
 </form></body></html>
@@ -65,7 +62,6 @@ FORM;
 $domain = $_POST['domain'];
 $vpnUsername = $_POST['vpnUsername'];
 $vpnSecret = $_POST['vpnSecret'];
-$vpnPassword = $_POST['vpnPassword'];
 
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename=' . $vpnUsername . '.mobileconfig');
@@ -75,13 +71,11 @@ echo str_replace(
         '{{DOMAIN}}',
         '{{VPNUSERNAME}}',
         '{{VPNSECRET}}',
-        '{{VPNPASSWORD}}',
     ],
     [
         $domain,
         $vpnUsername,
         $vpnSecret,
-        $vpnPassword
     ],
     $template
 );
